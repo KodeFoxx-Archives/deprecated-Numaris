064f7d6e3b41a132a453abc81b6526d63e91b4d2
Yves Schelpe <yves.schelpe@gmail.com>
2017-10-20 10:54:09 +0200

    Covered basics of parsing.

---
+++ Source/Backend/Kf.Numaris.Api.Tests/KdgStudentNumber/KdgStudentNumberParserTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Kf.Numaris.Api.Tests.KdgStudentNumber
+{
+    public class KdgStudentNumberParserTests
+    {
+        public static IEnumerable<object[]> Parser_can_handle_scenario_TestData()
+        {
+            yield return new object[] {
+                "Where number contains hyphen",
+                "0033311-40", new [] { "0033311", "40" }
+            };
+
+            yield return new object[] {
+                "Where number contains hyphen, but no trailing zeroes",
+                "33311-40", new [] { "33311", "40" }
+            };
+
+            yield return new object[] {
+                "Where number contains hyphen, and only one number before hyphen",
+                "1-40", new [] { "1", "40" }
+            };
+
+            yield return new object[] {
+                "Where number doesn't contain a hyphen",
+                "003331140", new [] { "0033311", "40" }
+            };
+
+            yield return new object[] {
+                "Where number doesn't contain a hyphen, and has no trailing zeroes",
+                "3331140", new [] { "33311", "40" }
+            };
+
+            yield return new object[] {
+                "Where number doesn't contain a hyphen, and consists of only three numbers in a whole",
+                "140", new [] { "1", "40" }
+            };
+        }
+
+        [Theory, MemberData(nameof(Parser_can_handle_scenario_TestData))]
+        public void Parser_can_handle_scenario(string scenario, string input, string[] expected)
+        {
+            var sut = new KdgStudentNumberParser();
+            var actual = sut.Parse(input);
+            Assert.Equal(actual, expected);
+        }
+
+        [Theory,
+         InlineData(""),
+         InlineData(" "),
+         InlineData("  "),
+         InlineData(null)]
+        public void Parser_throws_NumberParserException_with_ArgumentNullExcpetion_when_input_is(string input)
+        {
+            var sut = new KdgStudentNumberParser();
+            var exception = Assert.Throws<NumberParserException>(() =>
+            {
+                sut.Parse(input);
+            });
+            Assert.Equal(typeof(ArgumentNullException), exception.InnerException.GetType());
+        }
+
+        [Theory,
+         InlineData("00333-11-40", "The input can only contain 0 or 1 hyphen"),
+         InlineData("0033311-", "The input needs at least one character before and after the hyphen"),
+         InlineData("-003331140", "The input needs at least one character before and after the hyphen"),
+         InlineData("-", "The input needs at least one character before and after the hyphen"),
+         InlineData("22", "The input needs to have at least three characters")]
+        public void Parser_throws_NumberParserException_when_input_is(string input, string partialExceptionMessage)
+        {
+            var sut = new KdgStudentNumberParser();
+            var exception = Assert.Throws<NumberParserException>(() =>
+            {
+                sut.Parse(input);
+            });
+            Assert.Contains(partialExceptionMessage, exception.Message);
+        }
+    }
+}
---
+++ Source/Backend/Kf.Numaris.Api/Number.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+
+namespace Kf.Numaris.Api
+{
+    public interface INumber<TNumber>
+        where TNumber : class, INumber<TNumber>
+    {
+    }
+
+    public abstract class Number<TNumber> : INumber<TNumber>
+        where TNumber : class, INumber<TNumber>
+    {
+        protected readonly INumberParser<TNumber> _numberParser;
+
+        protected Number(INumberParser<TNumber> numberParser)
+            => _numberParser = numberParser ?? throw new ArgumentNullException(nameof(numberParser));
+    }
+
+    public sealed class KdgStudentNumber : Number<KdgStudentNumber>
+    {
+        public KdgStudentNumber(INumberParser<KdgStudentNumber> numberParser)
+            : base(numberParser)
+        {
+        }
+    }
+
+    public interface INumberParser<TNumber>
+        where TNumber : class, INumber<TNumber>
+    {
+        string[] Parse(string input);
+    }
+
+    public abstract class NumberParser<TNumber> : INumberParser<TNumber>
+        where TNumber : class, INumber<TNumber>
+    {
+        protected readonly string _baseExceptionString = $"Unable to parse given input to {typeof(TNumber).Name}.";
+
+        public virtual string[] Parse(string input) =>
+            throw new NotImplementedException($"{typeof(TNumber).Name} needs to have a parser implemented.");
+    }
+
+    public class NumberParserException : Exception
+    {
+        public NumberParserException(Exception exception)
+            : base(exception.Message, exception) { }
+        public NumberParserException(string message)
+            : base(message) { }
+        public NumberParserException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+
+    public sealed class KdgStudentNumberParser : NumberParser<KdgStudentNumber>
+    {
+        public override string[] Parse(string input)
+        {
+            if (String.IsNullOrWhiteSpace(input))
+                throw new NumberParserException(new ArgumentNullException(nameof(input)));
+
+            if (CanParseWhenInputContainsHyphen(input, out var parsedResult))
+                return parsedResult;
+
+            if (CanParseWhenInputIsAlphanumeric(input, out parsedResult))
+                return parsedResult;
+
+            throw new NumberParserException(_baseExceptionString);
+        }
+
+        private bool CanParseWhenInputContainsHyphen(string input, out string[] parsedResult)
+        {
+            parsedResult = null;
+
+            if (!input.Contains("-"))
+                return false;
+
+            if (input.Count(c => c == '-') > 1)
+                throw new NumberParserException($"{_baseExceptionString} The input can only contain 0 or 1 hyphen.");
+
+            var splittedInput = input.Split(new[] { "-" }, StringSplitOptions.None);
+
+            if (String.IsNullOrEmpty(splittedInput[0]) || String.IsNullOrWhiteSpace(splittedInput[1]))
+                throw new NumberParserException($"{_baseExceptionString} The input needs at least one character before and after the hyphen.");
+
+            parsedResult = splittedInput;
+            return true;
+        }
+
+        private bool CanParseWhenInputIsAlphanumeric(string input, out string[] parsedResult)
+        {
+            parsedResult = null;
+
+            if (input.Length < 3)
+                throw new NumberParserException($"{_baseExceptionString} The input needs to have at least three characters.");
+
+            parsedResult = new List<string> {
+                input.Substring(0, input.Length-2),
+                String.Join(String.Empty, input.Skip(input.Length-2))
+            }.ToArray();
+            return true;
+        }
+    }
+}
