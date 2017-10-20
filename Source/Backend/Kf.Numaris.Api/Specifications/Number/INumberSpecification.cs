c676b8f8672abe0c148fdd29c10e10551cab3501
Yves Schelpe <yves.schelpe@gmail.com>
2017-10-20 14:09:42 +0200

    Reboot, reset, relay

--- Source/Backend/Kf.Numaris.Api/Specifications/Number/INumberSpecification.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Kf.Numaris.Api.Specifications.Number
-{
-    public interface INumberSpecification<TNumberSpecification>
-        where TNumberSpecification : class, INumberSpecification<TNumberSpecification>
-    {
-    }
-}
