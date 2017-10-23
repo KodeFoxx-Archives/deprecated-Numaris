1cf47162e49fe19177168590f39cbd75ccd3f080
Yves Schelpe <yves.schelpe@sdworx.com>
2017-10-23 14:47:45 +0200

    Added example of a generic module loader, to make registrations for implementations easier.

---
+++ Source/Backend/Kf.Numaris.Implementations/AutofacNumarisModuleLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Autofac;
+using Kf.Numaris.Api.Formatting.Fields;
+using Kf.Numaris.Api.Formatting.Numbers;
+using Kf.Numaris.Api.Specifications.Field;
+using Kf.Numaris.Api.Specifications.Numbers;
+using Kf.Numaris.Implementations.KdgPersonNumber.Numbers;
+
+namespace Kf.Numaris.Implementations
+{
+    public abstract class NumarisAutofacModule<TNumberSpecification> : Module
+        where TNumberSpecification : INumberSpecification
+    {
+        protected override void Load(ContainerBuilder builder)            
+        {
+            builder.RegisterAssemblyTypes(typeof(KdgNumberSpecification).Assembly)
+                .Where(t => t.GetInterfaces().Contains(typeof(INumberFormatter<TNumberSpecification>)))
+                .Where(t => t.IsClass)
+                .AsImplementedInterfaces();
+
+            builder.RegisterAssemblyTypes(typeof(KdgNumberSpecification).Assembly)
+                .Where(t => t.GetInterfaces().Contains(typeof(IFieldSpecification<TNumberSpecification>)))
+                .Where(t => t.IsClass)
+                .AsImplementedInterfaces();
+
+            builder.RegisterAssemblyTypes(typeof(KdgNumberSpecification).Assembly)
+                .Where(t => t.GetInterfaces().Contains(typeof(IFieldFormatter<TNumberSpecification>)))
+                .Where(t => t.IsClass)
+                .AsImplementedInterfaces();
+        }
+    }
+}
--- Source/Backend/Kf.Numaris.Implementations/KdgPersonNumber/KdgPersonNumberAutofacModule.cs
+++ Source/Backend/Kf.Numaris.Implementations/KdgPersonNumber/KdgPersonNumberAutofacModule.cs
@@ -1,21 +1,8 @@
-﻿using Autofac;
-using Kf.Numaris.Api.Formatting.Fields;
-using Kf.Numaris.Api.Formatting.Numbers;
-using Kf.Numaris.Api.Specifications.Field;
-using Kf.Numaris.Implementations.KdgPersonNumber.Fields;
-using Kf.Numaris.Implementations.KdgPersonNumber.Numbers;
+﻿using Kf.Numaris.Implementations.KdgPersonNumber.Numbers;
 
 namespace Kf.Numaris.Implementations.KdgPersonNumber
 {
-    public class KdgPersonNumberAutofacModule : Module
+    public class KdgPersonNumberAutofacModule : NumarisAutofacModule<KdgNumberSpecification>
     {
-        protected override void Load(ContainerBuilder builder)
-        {
-            builder.RegisterType<KdgNumberFormatter>().As<INumberFormatter<KdgNumberSpecification>>();
-            builder.RegisterType<CheckDigitsFieldSpecification>().As<IFieldSpecification<KdgNumberSpecification>>();
-            builder.RegisterType<PersonNumberFieldSpecification>().As<IFieldSpecification<KdgNumberSpecification>>();
-            builder.RegisterType<PersonNumberFieldFormatter>().As<IFieldFormatter<KdgNumberSpecification>>();
-            builder.RegisterType<CheckDigitsFieldFormatter>().As<IFieldFormatter<KdgNumberSpecification>>();
-        }
     }
 }
