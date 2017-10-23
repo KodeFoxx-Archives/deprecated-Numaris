dd23dfd6d56dd39c00516404c045de49109f59d2
Yves Schelpe <yves.schelpe@sdworx.com>
2017-10-23 14:27:31 +0200

    Example of how to register the types with DI (container used in the example is Autofac).

--- Source/Frontend/Kf.Numaris.Presentation.ConsoleApplicationExample/FormattingExample.cs
+++ Source/Frontend/Kf.Numaris.Presentation.ConsoleApplicationExample/FormattingExample.cs
@@ -1,18 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using Kf.Numaris.Api.Formatting.Numbers;
 using Kf.Numaris.Implementations.KdgPersonNumber.Numbers;
 
 namespace Kf.Numaris.Presentation.ConsoleApplicationExample
 {
     class FormattingExample : IExample
     {
-        private readonly string[] _numbers = { "3331140", "003331140" };
+        private readonly INumberFormatter<KdgNumberSpecification> _numberFormatter;
+        private readonly List<string[]> _numbers = new List<string[]>
+        {
+            new []{ "33311", "4" },
+            new []{ "33311", "40" },
+            new []{ "0033311", "40" }
+        };
+
+        public FormattingExample(INumberFormatter<KdgNumberSpecification> numberFormatter)
+            => _numberFormatter = numberFormatter;        
 
         public void Run()
         {
             _numbers.ToList().ForEach(number =>
             {
-                Console.WriteLine($"{number}");
+                Console.WriteLine($"- {String.Join(" and ", number.Select(n => $"'{n}'"))}' formatted becomes '{_numberFormatter.Format(number)}'.");
             });
         }
     }
--- Source/Frontend/Kf.Numaris.Presentation.ConsoleApplicationExample/Program.cs
+++ Source/Frontend/Kf.Numaris.Presentation.ConsoleApplicationExample/Program.cs
@@ -3,23 +3,57 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using Autofac;
+using Autofac.Core;
+using Kf.Numaris.Api.Formatting.Fields;
+using Kf.Numaris.Api.Formatting.Numbers;
+using Kf.Numaris.Api.Specifications.Field;
+using Kf.Numaris.Implementations.KdgPersonNumber.Fields;
 
 namespace Kf.Numaris.Presentation.ConsoleApplicationExample
 {
     class Program
     {
+        private readonly IEnumerable<IExample> _examples;
+
         static void Main(string[] args)
         {
-            var program = new Program();
+            var container = ConfigureContainer();
+            var program = container.Resolve<Program>();  
+            
             program.RunExamples();
 
             Console.ReadKey();
         }
 
+        private static IContainer ConfigureContainer()
+        {
+            var builder = new ContainerBuilder();            
+
+            // This should be inside a seperate KdgNumberSpecification Module
+            builder.RegisterType<KdgNumberFormatter>().As<INumberFormatter<KdgNumberSpecification>>();
+            builder.RegisterType<CheckDigitsFieldSpecification>().As<IFieldSpecification<KdgNumberSpecification>>();
+            builder.RegisterType<PersonNumberFieldSpecification>().As<IFieldSpecification<KdgNumberSpecification>>();
+            builder.RegisterType<PersonNumberFieldFormatter>().As<IFieldFormatter<KdgNumberSpecification>>();
+            builder.RegisterType<CheckDigitsFieldFormatter>().As<IFieldFormatter<KdgNumberSpecification>>();
+
+            // This is application specific
+            builder.RegisterType<Program>();
+            builder.RegisterAssemblyTypes(typeof(Program).Assembly)
+                .Where(t => t.GetInterfaces().Contains(typeof(IExample)))
+                .Where(t => t.IsClass)
+                .AsImplementedInterfaces();
+
+            return builder.Build();
+        }
+
+        public Program(IEnumerable<IExample> examples)
+            => _examples = examples;        
+
         private void RunExamples()
-            => GetExamples().ToList()
+            => _examples.ToList()
                 .ForEach(e =>
-                {
+                {                    
                     ExecuteBeforeExample(e);
                     e.Run();
                     ExecuteAfterExample();
@@ -29,12 +63,6 @@ namespace Kf.Numaris.Presentation.ConsoleApplicationExample
             => Console.WriteLine($"Running '{example.GetType().Name}'.");
 
         private void ExecuteAfterExample(IExample example = null)
-            => Enumerable.Range(0, 2).ToList().ForEach(_ => Console.WriteLine());
-
-        private IEnumerable<IExample> GetExamples()
-            => typeof(Program).Assembly.GetTypes()
-                .Where(t => t.GetInterfaces().Contains(typeof(IExample)))
-                .Where(t => t.IsClass)
-                .Select(t => Activator.CreateInstance(t) as IExample);
+            => Enumerable.Range(0, 2).ToList().ForEach(_ => Console.WriteLine());                
     }
 }
