09a82631540b3dfa6ab5bd06b29ff0242139c7b1
Yves Schelpe <yves.schelpe@gmail.com>
2017-10-26 23:11:40 +0200

    Dynamically add tabs based on the applicationinfoviewmodel. #7

--- Source/Presentation/Kf.Numaris.Presentation.WpfClient/MainWindow.xaml.cs
+++ Source/Presentation/Kf.Numaris.Presentation.WpfClient/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
 using Kf.Numaris.Presentation.WpfClient.ViewModel;
 
 namespace Kf.Numaris.Presentation.WpfClient
@@ -9,11 +11,34 @@ namespace Kf.Numaris.Presentation.WpfClient
     public partial class MainWindow : Window
     {
         private readonly ApplicationInfoViewModel _applicationInfo;
+        private TabControl _tabControl;
 
         public MainWindow(ApplicationInfoViewModel applicationInfo)
         {
             _applicationInfo = applicationInfo;
             InitializeComponent();
+
+            BuildTabControl(applicationInfo);
+        }
+
+        private void BuildTabControl(ApplicationInfoViewModel applicationInfo)
+        {
+            _tabControl = new TabControl();
+
+            _tabControl.Items.Add(new TabItem
+            {
+                Header = new Label { Content = "ApplicationInfo" }
+            });
+
+            applicationInfo.NumberSpecifications.ToList().ForEach(numberSpecification =>
+            {
+                _tabControl.Items.Add(new TabItem
+                {
+                    Header = new Label { Content = numberSpecification.Identifier.Name }
+                });
+            });
+
+            this.Content = _tabControl;
         }
     }
 }
