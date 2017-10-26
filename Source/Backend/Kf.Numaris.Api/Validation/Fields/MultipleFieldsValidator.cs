3e235be1420618ee13d41df07e8fbfa623003cc0
Yves Schelpe <yves.schelpe@sdworx.com>
2017-10-26 08:44:39 +0200

    Added abstract base implementation for MultipleFieldsValidator. #1

---
+++ Source/Backend/Kf.Numaris.Api/Validation/Fields/MultipleFieldsValidator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Kf.Numaris.Api.Common;
+using Kf.Numaris.Api.Specifications.Field;
+using Kf.Numaris.Api.Specifications.Numbers;
+using Kf.Numaris.Api.Validation.Results;
+
+namespace Kf.Numaris.Api.Validation.Fields
+{
+    [DebuggerDisplay("{Identifier.Name} for #{FieldSpecificationIdentifiers.Count} fields")]
+    public abstract class MultipleFieldsValidator<TNumberSpecification> : IMultipleFieldsValidator<TNumberSpecification>
+        where TNumberSpecification : INumberSpecification
+    {
+        public Identifier Identifier
+            => Identifier.For<IMultipleFieldsValidator<TNumberSpecification>>(GetType());
+
+        public IReadOnlyList<Identifier> FieldSpecificationIdentifiers
+            => FieldSpecifications.Select(fs => fs.Identifier).ToList();
+
+        protected abstract IEnumerable<IFieldSpecification<TNumberSpecification>> FieldSpecifications { get; }
+
+        public abstract IFieldValidationResult<TNumberSpecification> Validate(IEnumerable<KeyValuePair<Identifier, string>> input);
+    }
+}
